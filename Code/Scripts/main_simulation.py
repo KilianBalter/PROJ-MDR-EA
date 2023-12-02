7be4444110ac558f72986e6deb4f68693feb959f
Jonas H <jonashess@tuta.com>
2023-12-02 09:55:06 +0100

    feat: adjust error handling
    moved json error from mdr_get_sys_name into main_simulation.py and did further error handling there

--- Code/Scripts/main_simulation.py
+++ Code/Scripts/main_simulation.py
@@ -1,8 +1,13 @@
 import json
-
+import logging
 import handle_data
 
 if __name__ == "__main__":
-    with open("../../Assets/Example MDR Events/MDR_Event_4.json") as json_file:
-        data = json.load(json_file)
-        print(handle_data.handle_data(data))
+    try:
+        with open("../../Assets/Example MDR Events/MDR_Event_1.json") as json_file:
+            data = json.load(json_file)
+            print(handle_data.handle_data(data))
+    except json.decoder.JSONDecodeError:
+        logging.error("Error in JSON file. Check for correct JSON syntax!")
+    except FileNotFoundError:
+        logging.error("File wasn't found. Check for the correct path!")
--- Code/Scripts/ticket_037.py
+++ Code/Scripts/ticket_037.py
@@ -12,7 +12,5 @@ def mdr_get_sys_name(mdr_event: str) -> str:
             ['host']['name']
     except KeyError:
         logging.error("Error in key path. Compare path in json file and in variable declaration!")
-    except json.decoder.JSONDecodeError:
-        logging.error("Error in JSON file. Check for correct JSON syntax!")
 
     return sys_name
