f47f577213cb9e737e68085e8aa2d0eda2698929
vaish4114 <68846571+vaish4114@users.noreply.github.com>
2023-12-07 18:31:20 +0100

    Update extractingeasystemid.py

--- src/extractingeasystemid.py
+++ src/extractingeasystemid.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import src.obtain_a_JWT_token as Jwt
 import receive_list_of_systems_from_ea as Los
 
@@ -14,24 +13,28 @@ def get_system_id(mdr_name, systems):
     # If MDR name is not found, return None or raise an exception based on your use case
     return None
 
-
 # Example usage:
 
-systems = Los.get_list_of_systems_from_ea(Jwt.get_jwt_token("","")).json()
+# Get the JWT token
+jwt_token = Jwt.get_jwt_token("your_username", "your_password").json()
 
+# Get the list of systems from the EA API
+systems_response = Los.get_list_of_systems_from_ea(jwt_token)
 
-mdr_name_input = "YourMDRName"
-systems_input = [
-    {"mdr_name": "System1MDR", "ea_system_id": "EA123"},
-    {"mdr_name": "System2MDR", "ea_system_id": "EA456"},
+# Check if the request was successful
+if systems_response.status_code == 200:
+    # Convert the JSON response to a list of systems
+    systems = systems_response.json()
 
-]
+    mdr_name_input = "YourMDRName"
 
-# Call the function
-ea_system_id = get_system_id(mdr_name_input, systems_input)
+    # Call the function
+    ea_system_id = get_system_id(mdr_name_input, systems)
 
-# Print the result
-if ea_system_id is not None:
-    print(f"EA System ID for {mdr_name_input}: {ea_system_id}")
+    # Print the result
+    if ea_system_id is not None:
+        print(f"EA System ID for {mdr_name_input}: {ea_system_id}")
+    else:
+        print(f"No matching system found for {mdr_name_input}")
 else:
-    print(f"No matching system found for {mdr_name_input}")
+    print(f"Error getting systems: {systems_response.status_code} - {systems_response.text}")
