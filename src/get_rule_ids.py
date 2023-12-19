df974bb79fcfd869152756841303879d780fa2b0
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2023-12-19 13:55:42 +0100

    PROJFBPRO-62 Implement logic for getting rule IDs from Hardening

---
+++ src/get_rule_ids.py
@@ -0,0 +1,29 @@
+from src.ea_rest_template import ea_rest_call
+
+
+# Retrieves list of IDs of all rules contained in a Hardening
+def get_rule_ids(token, hardening_id) -> list[int]:
+    # Get information about the specific hardening (included benchmarks, excluded rules, etc...)
+    hardening_info = ea_rest_call(f"/api/v3/hardeningengine/Hardenings/{hardening_id}", "GET", token)
+
+    # Filter the response to only benchmarks and excluded rules for the HardeningWizard
+    keys = ['includes', 'conflictExcludes', 'gpoExcludes', 'manualExcludes']
+    wizard_body = {key: hardening_info[key] for key in keys}
+
+    # Append some additional required fields
+    # Just assigning pageSize a very big value to get all rules every time
+    wizard_body.update({
+        'benchmarkId': None,
+        'onlyExcluded': False,
+        'pageSize': 1_000_000,
+        'refIdFilter': None,
+        'startIndex': 0,
+        'titleFilter': None})
+
+    # Retrieve rule information from the HardeningWizard
+    rules = ea_rest_call("/api/v3/hardeningengine/HardeningWizard/rules", 'POST', token, data=wizard_body)
+
+    # Filter out only the rule IDs
+    rule_ids = [rule['includedBy'][0]['ruleId'] for rule in rules['items']]
+
+    return rule_ids
