930b0607c05b742d718fc92d112fb37d0ede46c5
vaish4114 <vaishuraja123@gmail.com>
2023-12-21 18:07:41 +0100

    PROJFBPRO-63 feat: Recieving attackID - Test

--- src/get_attack_id_from_event.py
+++ src/get_attack_id_from_event.py
@@ -4,7 +4,7 @@ import logging
 def get_attack_id(mdr_event) -> str:  # Returns the attackId from the MDR event
     attack_id = None
     try:
-        attack_id = mdr_event['event']['action']['details']['technique']['id']
+        attack_id = mdr_event['event']['taxonomy']['External']['Attack']['action']['details']['technique']['id']
     except KeyError:
         logging.error("Error in key path. Compare path in the JSON file and in variable declaration!")
     return attack_id
