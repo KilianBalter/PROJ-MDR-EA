aaf25acbfb77081e09ed4908cd958bc1f8945336
Espen-Balcar <E.Balcar@gmx.net>
2023-12-21 10:19:44 +0100

    PROJFBPRO-55 Base adjusted get_rule_by_id.py, so that it does work correctly with receive_the_instance_name.py

--- src/get_rule_by_id.py
+++ src/get_rule_by_id.py
@@ -1,14 +1,18 @@
-import requests
 import json
+
 from src.ea_rest_template import ea_rest_call
 
 
 def get_rule_by_instance_name(token, instance_name):
-    data = {'instanceNames': [instance_name]}
-    response = ea_rest_call(f'/api/v3/hardeningengine/HardeningDsc/instanceRules', 'POST',  token, None, data)
-    return get_rule_by_rule_id(token, json.dumps(response['resolvedRules'][0]['includedBy'][0]['ruleId']))
+    if isinstance(instance_name, list):
+        data = {'instanceNames': instance_name}
+    else:
+        data = {'instanceNames': [instance_name]}
+    response = ea_rest_call(f'/api/v3/hardeningengine/HardeningDsc/instanceRules', 'POST', token, None, data)
+    for i in range(len(response['resolvedRules'])):
+        print(get_rule_by_rule_id(token, json.dumps(response['resolvedRules'][i]['includedBy'][0]['ruleId'])))
 
 
 def get_rule_by_rule_id(token: str, rule_id: str):
-    response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/'+rule_id, 'GET',  token, None)
+    response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/' + rule_id, 'GET', token, None)
     return json.dumps(response['settings'][0])
