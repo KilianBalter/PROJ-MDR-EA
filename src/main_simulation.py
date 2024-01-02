34a9a53b0e8ef662440bbd1b34905b7c55e0bc88
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-02 09:42:15 +0100

    Implement simulation with 2 test events

--- src/main_simulation.py
+++ src/main_simulation.py
@@ -5,9 +5,19 @@ from src.handle_data import handle_data
 
 if __name__ == "__main__":
     try:
-        with open("../assets/Example MDR Events/Test_Event_1.json") as event_file:
-            event = json.load(event_file)
-            handle_data(event)
+        files = ["Test_Event_1.json", "Test_Event_2.json"]
+        for file in files:
+            with open("../assets/Example MDR Events/" + file) as event_file:
+                print(file + ":")
+                json_event = json.load(event_file)
+                sat_mitigations, vuln_status = handle_data(json_event)
+
+                print(f"Vulnerability status: {vuln_status}")
+                if sat_mitigations:
+                    print(f"Following mitigations are present: {sat_mitigations}")
+                else:
+                    print("No mitigations are present")
+                print("\n\n")
     except json.decoder.JSONDecodeError:
         logging.error("Error in JSON file. Check for correct JSON syntax!")
     except FileNotFoundError:
