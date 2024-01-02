69ec30913661d68f7b5e571cc5457533898547a0
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-03 00:58:45 +0100

    PROJFBPRO-64 Implement logic for adding mitigation status to event

---
+++ src/add_mitigation_status.py
@@ -0,0 +1,44 @@
+from ea_rest_template import ea_rest_call
+
+
+def add_mitigation_status(token, event, satisfied_mitigations, unsatisfied_mitigations, error_message=""):
+    # If EA reachable, get rule titles and add them as additional info
+    if token:
+        add_rule_titles(satisfied_mitigations, token)
+        add_rule_titles(unsatisfied_mitigations, token)
+
+    # Determine vulnerability status
+    vuln_status = "NOT_VULNERABLE" if satisfied_mitigations else "VULNERABLE"
+
+    # Add information to event
+    event.update({
+        "hardening_info": {
+            # Mitigations for which all mapped rules are (not) present on the system
+            # including descriptions for each mitigation and rule
+            "satisfied_mitigations": satisfied_mitigations,
+            "unsatisfied_mitigations": unsatisfied_mitigations,
+
+            # Estimated vulnerability of the system
+            "vulnerability_status": vuln_status,
+
+            # Additional information about errors that occurred during lookup
+            "error_message": error_message
+        }
+    })
+
+
+def add_rule_titles(mitigations, token):
+    updated_rules = {}
+
+    for mitigation in mitigations:
+        for rule in mitigations[mitigation]['rules']:
+            response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/{rule}', 'GET', token)
+            updated_rules.update({
+                rule: {
+                    'title': response['title']
+                }
+            })
+
+        mitigations[mitigation].update({
+            "rules": updated_rules
+        })
