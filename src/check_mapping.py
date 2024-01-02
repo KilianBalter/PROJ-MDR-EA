701d187d6dda53e7cad88d4de908361750cc39c6
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-03 00:57:39 +0100

    PROJFBPRO-64 Add error handling and modify the return to include full mitigation, not just ID

--- src/check_mapping.py
+++ src/check_mapping.py
@@ -4,16 +4,18 @@ import json
 def check_mapping(attack_id, rule_ids):
     with open("../assets/mapping.json", 'r') as f:
         mapping = json.load(f)
-        satisfied_mitigations = []
-        try:
-            mitigations = mapping['techniques'][f'{attack_id}']['mitigations']
+        if attack_id not in mapping['techniques']:
+            raise LookupError(f"Technique {attack_id} has not been mapped")
 
-            for mitigation in mitigations:
-                rules = mitigations[mitigation]['rules']
-                if all(_id in rule_ids for _id in rules):
-                    satisfied_mitigations.append(mitigation)
-        except KeyError:  # Technique has not been mapped
-            # Simply return empty satisfied_mitigations array
-            pass
+        satisfied_mitigations = {}
+        unsatisfied_mitigations = {}
+        mitigations = mapping['techniques'][f'{attack_id}']['mitigations']
 
-        return satisfied_mitigations
+        for mitigation_id in mitigations:
+            rules = mitigations[mitigation_id]['rules']
+            if all(_id in rule_ids for _id in rules):
+                satisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
+            else:
+                unsatisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
+
+        return satisfied_mitigations, unsatisfied_mitigations
