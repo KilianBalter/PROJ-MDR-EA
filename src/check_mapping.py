18da3feb15c3d4e31bbff15c6df362a3b29d8c3f
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-03 22:57:00 +0100

    PROJFBPRO-64 Get mitigations as input instead. Logic of getting mitigations will be in handle_data

--- src/check_mapping.py
+++ src/check_mapping.py
@@ -1,21 +1,15 @@
 import json
 
 
-def check_mapping(attack_id, rule_ids):
-    with open("../assets/mapping.json", 'r') as f:
-        mapping = json.load(f)
-        if attack_id not in mapping['techniques']:
-            raise LookupError(f"Technique {attack_id} has not been mapped")
+def check_mapping(mitigations, rule_ids):
+    satisfied_mitigations = {}
+    unsatisfied_mitigations = {}
 
-        satisfied_mitigations = {}
-        unsatisfied_mitigations = {}
-        mitigations = mapping['techniques'][f'{attack_id}']['mitigations']
+    for mitigation_id in mitigations:
+        rules = mitigations[mitigation_id]['rules']
+        if all(_id in rule_ids for _id in rules):
+            satisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
+        else:
+            unsatisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
 
-        for mitigation_id in mitigations:
-            rules = mitigations[mitigation_id]['rules']
-            if all(_id in rule_ids for _id in rules):
-                satisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
-            else:
-                unsatisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
-
-        return satisfied_mitigations, unsatisfied_mitigations
+    return satisfied_mitigations, unsatisfied_mitigations
