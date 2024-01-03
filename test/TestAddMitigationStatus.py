07f65a002312acf20e82630cb7ef550cef968156
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-03 17:24:35 +0100

    PROJFBPRO-64 Add tests for add_mitigation_status

---
+++ test/TestAddMitigationStatus.py
@@ -0,0 +1,58 @@
+import unittest
+import json
+
+from src.my_credentials import get_credentials
+from src.obtain_a_JWT_token import get_jwt_token
+from src.add_mitigation_status import add_mitigation_status
+
+
+class TestAddMitigationStatus(unittest.TestCase):
+    def setUp(self):
+        print("\nStarting Unittest.\n")
+        with open("../assets/Example MDR Events/Test_Event_1.json") as self.json_file_1, \
+                open("../assets/Example MDR Events/Test_Event_2.json") as self.json_file_2:
+            self.data_1 = json.load(self.json_file_1)
+            self.data_2 = json.load(self.json_file_2)
+
+    def tearDown(self):
+        self.json_file_2.close()
+        self.json_file_1.close()
+        print("\nEnding Unittest\n")
+
+    def test_add_mitigation_status(self):
+        credentials = get_credentials()
+        token = get_jwt_token(credentials['username'], credentials['password'])
+
+        hardening_info = {
+            "satisfied_mitigations": {
+                "M1040": {
+                    "description": "On Windows 10, enable Attack Surface Reduction (ASR) rules to prevent Visual Basic and JavaScript scripts from executing potentially malicious downloaded content.",
+                    "rules": {
+                        1608: {
+                            "title": "(L1) Ensure 'Configure Attack Surface Reduction rules: Block JavaScript or VBScript from launching downloaded executable content'"
+                        }
+                    }
+                }
+            },
+            "unsatisfied_mitigations": {},
+            "vulnerability_status": "NOT_VULNERABLE",
+            "error_message": ''
+        }
+
+        satisfied_mitigations = {
+            'M1040': {
+                "description": "On Windows 10, enable Attack Surface Reduction (ASR) rules to prevent Visual Basic and JavaScript scripts from executing potentially malicious downloaded content.",
+                'rules': [1608]
+            }
+        }
+        unsatisfied_mitigations = {}
+
+        add_mitigation_status(token, self.data_1, satisfied_mitigations, unsatisfied_mitigations)
+
+        rules = self.data_1['hardening_info']['satisfied_mitigations']['M1040']['rules']
+        self.assertTrue(all(rules[rule]['title'] for rule in rules), "Missing titles")
+        self.assertEqual(hardening_info, self.data_1['hardening_info'], "Hardening info doesn't match")
+
+
+if __name__ == '__main__':
+    unittest.main()
