c3ba53ed20dd959135bfe8f0eb9ceb2fe9ecb145
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-03 17:52:12 +0100

    PROJFBPRO-59 Add basic error handling to use add_mitigation_status

--- src/handle_data.py
+++ src/handle_data.py
@@ -9,27 +9,40 @@ from src.get_hardening_id import get_hardening_id
 from src.get_rule_ids import get_rule_ids
 from src.get_the_state_of_the_system import get_the_state
 from check_mapping import check_mapping
+from add_mitigation_status import add_mitigation_status
 
 
 def handle_data(event):
-    vuln_status = True
+    token = None
+    satisfied_mitigations = None
+    unsatisfied_mitigations = None
+    error_message = None
 
-    credentials = get_credentials()
-    token = get_jwt_token(credentials['username'], credentials['password'])
-    mdr_name = get_mdr_sys_name(event)
-    attack_id = get_attack_id(event)
-    systems = get_all_systems(token)
-    ea_system_id = get_ea_system_id_from_list_of_systems(systems, mdr_name)
+    try:
+        credentials = get_credentials()
+        token = get_jwt_token(credentials['username'], credentials['password'])
+        mdr_name = get_mdr_sys_name(event)
+        attack_id = get_attack_id(event)
+        systems = get_all_systems(token)
+        ea_system_id = get_ea_system_id_from_list_of_systems(systems, mdr_name)
 
-    state = get_the_state(token, ea_system_id)
-    if not state:
-        return event, vuln_status
+        # state = get_the_state(token, ea_system_id)
+        # if not state:
+        #     add_mitigation_status(token, event, satisfied_mitigations, unsatisfied_mitigations, "System is not in state")
+        #     return event
 
-    variation_key = get_hardening_variation_key_by_id(token, ea_system_id)
-    hardening_id = get_hardening_id(token, variation_key)
-    rules = get_rule_ids(token, hardening_id)
-    mitigations = check_mapping(attack_id, rules)
-    if mitigations:
-        vuln_status = False
+        variation_key = get_hardening_variation_key_by_id(token, ea_system_id)
+        hardening_id = get_hardening_id(token, variation_key)
+        rules = get_rule_ids(token, hardening_id)
+        satisfied_mitigations, unsatisfied_mitigations = check_mapping(attack_id, rules)
 
-    return mitigations, vuln_status
+    except LookupError as e:
+        if e.args:
+            error_message = e.args[0]
+    except Exception as e:
+        if e.args:
+            error_message = e.args[0]
+
+    add_mitigation_status(token, event, satisfied_mitigations, unsatisfied_mitigations, error_message)
+
+    return event
