704ddf71d491d5baa2cd4e595aff3d59e7c11b45
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-07 16:50:35 +0100

    PROJFBPRO-64 Update fields individually, so fields that aren't being updated aren't overriden

--- src/update_mitigation_status.py
+++ src/update_mitigation_status.py
@@ -12,36 +12,21 @@ def update_mitigation_status(event, token, satisfied_mitigations=None, unsatisfi
     if satisfied_mitigations is not None:
         vuln_status = "NOT_VULNERABLE" if satisfied_mitigations else "VULNERABLE"
 
-    # Gather all fields to be updated
-    updated_fields = {}
-
+    # Update fields
     # Estimated vulnerability of the system
     if vuln_status is not None:
-        updated_fields.update({
-            "vulnerability_status": vuln_status
-        })
+        event["hardening_info"]['vulnerability_status'] = vuln_status
 
-    # Mitigations for which all mapped rules are (not) present on the system
+    # Mitigations for which (not) all mapped rules are present on the system,
     # including descriptions for each mitigation and rule
     if satisfied_mitigations is not None:
-        updated_fields.update({
-            "satisfied_mitigations": satisfied_mitigations
-        })
+        event["hardening_info"]['satisfied_mitigations'] = satisfied_mitigations
     if unsatisfied_mitigations is not None:
-        updated_fields.update({
-            "unsatisfied_mitigations": unsatisfied_mitigations
-        })
+        event["hardening_info"]['unsatisfied_mitigations'] = unsatisfied_mitigations
 
     # Additional information about errors that occurred during lookup
     if error_message is not None:
-        updated_fields.update({
-            "error_message": error_message
-        })
-
-    # Update fields in event
-    event.update({
-        "hardening_info": updated_fields
-    })
+        event["hardening_info"]['error_message'] = error_message
 
 
 def add_rule_titles(mitigations, token):
