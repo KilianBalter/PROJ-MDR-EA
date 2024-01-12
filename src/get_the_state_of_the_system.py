ffc97fb0157b3b3aa7be9acfdb9cf4c7da2848bb
Quang Nguyen <quang.nguyen@fb-pro.com>
2024-01-12 22:59:47 +0100

    PROJFBPRO-66: Add Errorhandling get_the_state_of_the_system

--- src/get_the_state_of_the_system.py
+++ src/get_the_state_of_the_system.py
@@ -4,13 +4,16 @@ import json
 
 # main function
 def get_the_state(token: str, server_id: str) -> str:
-    if not server_id:
-        return ''
-    # extract dscServerId
-    response = ea_rest_call(f'/api/dtools/DscServer/{server_id}', 'GET', token)
-    dsc_server_id = response["serverId"]
-    # use dscServerId to get the ApplicationStatus
-    response2 = ea_rest_call(f'/api/jobProcessing/DscServerApplicationStatus/dscserver/{dsc_server_id}', 'GET', token)
-    # extract state_information and change the JSON-string to a python-dictionary
-    state_information = json.loads(response2[0]['stateDetails'])
+    try:
+        if not server_id:
+            return ''
+        # extract dscServerId
+        response = ea_rest_call(f'/api/dtools/DscServer/{server_id}', 'GET', token)
+        dsc_server_id = response["serverId"]
+        # use dscServerId to get the ApplicationStatus
+        response2 = ea_rest_call(f'/api/jobProcessing/DscServerApplicationStatus/dscserver/{dsc_server_id}', 'GET', token)
+        # extract state_information and change the JSON-string to a python-dictionary
+        state_information = json.loads(response2[0]['stateDetails'])
+    except Exception:
+        raise Exception("error in get_the_state")
     return state_information['InState']
