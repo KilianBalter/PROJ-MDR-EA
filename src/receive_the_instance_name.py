8c57a3fa17f68d60bfc4b9b17ca3018e2d8f6db3
Quang Nguyen <quang.nguyen@fb-pro.com>
2024-01-12 23:13:32 +0100

    PROJFBPRO-66: Add Errorhandling receive_the_instance_name.py

--- src/receive_the_instance_name.py
+++ src/receive_the_instance_name.py
@@ -7,14 +7,17 @@ def receive_instance_name(code_template: str) -> str | list[str]:
     :param code_template:
     :return: empty String if there is no code_template, list of instance_names in the code_template otherwise
     """
-    if not code_template:
-        return ''
-    lines = code_template.splitlines()
-    # filtering the lines to only work with the lines that contain xRegistry
-    x_reg = [x for x in lines if "xRegistry" in x]
-    # converting the array into a single string so that
-    x_reg_str = ''.join(x_reg)
-    return find_all_instance_names(x_reg_str)
+    try:
+        if not code_template:
+            return ''
+        lines = code_template.splitlines()
+        # filtering the lines to only work with the lines that contain xRegistry
+        x_reg = [x for x in lines if "xRegistry" in x]
+        # converting the array into a single string so that
+        x_reg_str = ''.join(x_reg)
+        return find_all_instance_names(x_reg_str)
+    except Exception:
+        raise Exception("error in receive_instance_name")
 
 
 def find_all_instance_names(text: str) -> list[str]:
@@ -22,4 +25,7 @@ def find_all_instance_names(text: str) -> list[str]:
     this function filters a String for every instance name with a RegEx
     """
     pattern = r'[A-Z0-9]{23,24}'
-    return [match.group() for match in re.finditer(pattern, text)]
+    try:
+        return [match.group() for match in re.finditer(pattern, text)]
+    except Exception:
+        raise Exception("error in find_all_instance_names")
