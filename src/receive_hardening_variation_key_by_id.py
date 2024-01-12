a4f7bfd6a372b1d8f1a5b0a4ebfedc32cdf42077
Quang Nguyen <quang.nguyen@fb-pro.com>
2024-01-12 23:10:51 +0100

    PROJFBPRO-66: Add Errorhandling receive_hardening_variation_key_by_id.py

--- src/receive_hardening_variation_key_by_id.py
+++ src/receive_hardening_variation_key_by_id.py
@@ -6,18 +6,27 @@ def get_hardening_variation_key_by_id(token: str, server_id: str) -> str:
     if not server_id:
         return ''
 
-    dsc_server_id = get_dsc_server_id(token, server_id)
-    dsc_server_application = get_dsc_server_application(token, dsc_server_id)
-    for dsc in dsc_server_application:
-        return dsc["variationKey"]
+    try:
+        dsc_server_id = get_dsc_server_id(token, server_id)
+        dsc_server_application = get_dsc_server_application(token, dsc_server_id)
+        for dsc in dsc_server_application:
+            return dsc["variationKey"]
+    except Exception:
+        raise Exception("error in get_hardening_variation_key_by_id")
 
 
 # Function to receive the dscServerId
 def get_dsc_server_id(token, id_1):
-    response = ea_rest_call(f'/api/dtools/DscServer/{id_1}', 'GET', token)
-    return response["serverId"]
+    try:
+        response = ea_rest_call(f'/api/dtools/DscServer/{id_1}', 'GET', token)
+        return response["serverId"]
+    except Exception:
+        raise Exception("error in get_dsc_server_id")
 
 
 # Function to receive the application information. Variation key is part of it
 def get_dsc_server_application(token, dsc_id):
-    return ea_rest_call(f'/api/dtools/DscServerApplication/dscserver/{dsc_id}', 'GET', token)
+    try:
+        return ea_rest_call(f'/api/dtools/DscServerApplication/dscserver/{dsc_id}', 'GET', token)
+    except Exception:
+        raise Exception("error in get_dsc_server_application")
