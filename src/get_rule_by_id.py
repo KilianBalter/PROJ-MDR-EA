a60c8558f3b3df7dc3472d72217f12a2b66e23c0
Quang Nguyen <quang.nguyen@fb-pro.com>
2024-01-12 22:56:19 +0100

    PROJFBPRO-66: Add Errorhandling get_rule_by_id

--- src/get_rule_by_id.py
+++ src/get_rule_by_id.py
@@ -4,15 +4,21 @@ from src.ea_rest_template import ea_rest_call
 
 
 def get_rule_by_instance_name(token, instance_name):
-    if isinstance(instance_name, list):
-        data = {'instanceNames': instance_name}
-    else:
-        data = {'instanceNames': [instance_name]}
-    response = ea_rest_call(f'/api/v3/hardeningengine/HardeningDsc/instanceRules', 'POST', token, None, data)
-    for i in range(len(response['resolvedRules'])):
-        print(get_rule_by_rule_id(token, json.dumps(response['resolvedRules'][i]['includedBy'][0]['ruleId'])))
+    try:
+        if isinstance(instance_name, list):
+            data = {'instanceNames': instance_name}
+        else:
+            data = {'instanceNames': [instance_name]}
+        response = ea_rest_call(f'/api/v3/hardeningengine/HardeningDsc/instanceRules', 'POST', token, None, data)
+        for i in range(len(response['resolvedRules'])):
+            print(get_rule_by_rule_id(token, json.dumps(response['resolvedRules'][i]['includedBy'][0]['ruleId'])))
+    except Exception:
+        raise Exception("error in get_rule_by_instance_name")
 
 
 def get_rule_by_rule_id(token: str, rule_id: str):
-    response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/' + rule_id, 'GET', token, None)
+    try:
+        response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/' + rule_id, 'GET', token, None)
+    except Exception:
+        raise Exception("error in get_rule_by_rule_id")
     return json.dumps(response['settings'][0])
