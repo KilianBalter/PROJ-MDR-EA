26b9589f5c6334522ad813d86c2e198cae13533d
Quang Nguyen <quang.nguyen@fb-pro.com>
2024-01-12 22:40:42 +0100

    PROJFBPRO-66: Add Errorhandling check_mapping

--- src/check_mapping.py
+++ src/check_mapping.py
@@ -12,4 +12,7 @@ def check_mapping(mitigations, rule_ids):
         else:
             unsatisfied_mitigations.update({mitigation_id: mitigations[mitigation_id]})
 
+    if not unsatisfied_mitigations and not satisfied_mitigations:
+        raise ValueError("no mitigations found")
+
     return satisfied_mitigations, unsatisfied_mitigations
