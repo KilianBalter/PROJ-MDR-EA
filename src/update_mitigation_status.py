bd661c819b3075c6d8fb15c615a3c6a27bb42b52
KilianBalter <94173233+KilianBalter@users.noreply.github.com>
2024-01-17 17:52:11 +0100

    PROJFBPRO-68 Add partial mitigation support to update_mitigation_status

--- src/update_mitigation_status.py
+++ src/update_mitigation_status.py
@@ -1,13 +1,18 @@
 from src.ea_rest_template import ea_rest_call
 
 
-def update_mitigation_status(event, token=None, satisfied_mitigations=None, unsatisfied_mitigations=None,
-                             error_message=None):
+def update_mitigation_status(event, token=None, _satisfied_mitigations=None, _partial_mitigations=None,
+                             _unsatisfied_mitigations=None, error_message=None):
+    satisfied_mitigations = _satisfied_mitigations.copy()
+    partial_mitigations = _partial_mitigations.copy()
+    unsatisfied_mitigations = _unsatisfied_mitigations.copy()
+
     # If EA reachable, get rule titles and add them as additional info
     if token:
         try:
-            add_rule_titles(satisfied_mitigations, token)
-            add_rule_titles(unsatisfied_mitigations, token)
+            add_rule_titles(satisfied_mitigations, False, token)
+            add_rule_titles(partial_mitigations, True, token)
+            add_rule_titles(unsatisfied_mitigations, False, token)
         # Titles serve only as additional information. If anything goes wrong due to EA, ignore and just update fields
         except Exception:
             pass
@@ -22,10 +27,12 @@ def update_mitigation_status(event, token=None, satisfied_mitigations=None, unsa
     if vuln_status is not None:
         event["hardening_info"]['vulnerability_status'] = vuln_status
 
-    # Mitigations for which (not) all mapped rules are present on the system,
+    # Mitigations for which all/some/none mapped rules are present on the system,
     # including descriptions for each mitigation and rule
     if satisfied_mitigations is not None:
         event["hardening_info"]['satisfied_mitigations'] = satisfied_mitigations
+    if partial_mitigations is not None:
+        event["hardening_info"]['partial_mitigations'] = partial_mitigations
     if unsatisfied_mitigations is not None:
         event["hardening_info"]['unsatisfied_mitigations'] = unsatisfied_mitigations
 
@@ -34,20 +41,29 @@ def update_mitigation_status(event, token=None, satisfied_mitigations=None, unsa
         event["hardening_info"]['error_message'] = error_message
 
 
-def add_rule_titles(mitigations, token):
+def add_rule_titles(mitigations, partial, token):
     if not mitigations:
         return
 
-    updated_rules = {}
-    for mitigation in mitigations:
-        for rule in mitigations[mitigation]['rules']:
-            response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/{rule}', 'GET', token)
-            updated_rules.update({
-                rule: {
-                    'title': response['title']
-                }
-            })
+    if partial:
+        updated_rules = {
+            'present': {},
+            'not_present': {}
+        }
+        for mitigation in mitigations:
+            for rule in mitigations[mitigation]['rules']['present']:
+                updated_rules['present'][rule] = {'title': get_rule_title(rule, token)}
+            for rule in mitigations[mitigation]['rules']['not_present']:
+                updated_rules['not_present'][rule] = {'title': get_rule_title(rule, token)}
+            mitigations[mitigation]['rules'] = updated_rules
+    else:
+        updated_rules = {}
+        for mitigation in mitigations:
+            for rule in mitigations[mitigation]['rules']:
+                updated_rules[rule] = {'title': get_rule_title(rule, token)}
+            mitigations[mitigation]['rules'] = updated_rules
 
-        mitigations[mitigation].update({
-            "rules": updated_rules
-        })
+
+def get_rule_title(rule, token):
+    response = ea_rest_call(f'/api/v3/benchmarkengine/Rules/{rule}', 'GET', token)
+    return response['title']
